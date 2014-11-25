924feeda7674ce172afe546044b32a3a8d533923
Daniel Mikusa <dmikusa@pivotal.io>
2014-11-25 16:41:22 -0500

    Added method to prompt user for y/n questions.  Including tests.

--- support_plugin.go
+++ support_plugin.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/cloudfoundry/cli/plugin"
 	"github.com/dmikusa-pivotal/support_plugin/ticket_log"
+	"io"
 	"io/ioutil"
+	"strings"
 )
 
 type SupportPlugin struct {
@@ -23,6 +26,26 @@ func runSingleCommand(cliConnection plugin.CliConnection, cmd string) (te ticket
 	return
 }
 
+func PromptForYesNo(reader io.Reader, question string) bool {
+	fmt.Println(question, "(y/n)")
+	cnt := 4
+	r := bufio.NewReader(reader)
+	line, err := r.ReadString('\n')
+	for cnt > 0 && err != nil {
+		fmt.Println("Sorry, I didn't get that.  Please try again.")
+		fmt.Println(question, "(y/n)")
+		line, err = r.ReadString('\n')
+		cnt -= 1
+	}
+	line = strings.TrimSpace(line)
+	fmt.Println("line:", line)
+	if cnt > 0 && (line == "y" || line == "Y") {
+		return true
+	} else {
+		return false
+	}
+}
+
 func (sp *SupportPlugin) Run(cliConnection plugin.CliConnection, args []string) {
 	fmt.Println("Don't Panic!  We're gathering some information. Please hold.\n")
 	sp.TicketLog = ticket_log.NewTicketLog()
@@ -30,8 +53,10 @@ func (sp *SupportPlugin) Run(cliConnection plugin.CliConnection, args []string) 
 	sp.TicketLog.Append(runSingleCommand(cliConnection, "apps"))
 	sp.TicketLog.Append(runSingleCommand(cliConnection, "services"))
 	fmt.Println("We've gathered the following information.  Please review.\n")
+	fmt.Println("-------------------------------------------------------------")
 	data, _ := ioutil.ReadFile(sp.TicketLog.Name)
 	fmt.Println(string(data[:]))
+	fmt.Println("-------------------------------------------------------------")
 	fmt.Println("")
 }
 
--- support_plugin_test.go
+++ support_plugin_test.go
@@ -6,6 +6,7 @@ import (
 	. "github.com/dmikusa-pivotal/support_plugin"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"io"
 )
 
 var _ = Describe("SupportPlugin", func() {
@@ -43,9 +44,46 @@ var _ = Describe("SupportPlugin", func() {
 			output := io_helpers.CaptureOutput(func() {
 				supportPlugin.Run(fakeCliConnection, []string{})
 			})
-			Expect(output[4]).To(ContainSubstring("## Output from `target`"))
-			Expect(output[8]).To(ContainSubstring("## Output from `apps`"))
-			Expect(output[12]).To(ContainSubstring("## Output from `services`"))
+			Expect(output).To(ContainElement(ContainSubstring("## Output from `target`")))
+			Expect(output).To(ContainElement(ContainSubstring("## Output from `apps`")))
+			Expect(output).To(ContainElement(ContainSubstring("## Output from `services`")))
+		})
+	})
+	Describe("PromptForYesNo", func() {
+		PromptForInput := func(input string) ([]string, bool) {
+			var answer bool
+			output := io_helpers.CaptureOutput(func() {
+				io_helpers.SimulateStdin(input, func(r io.Reader) {
+					answer = PromptForYesNo(r, "Is this a dumb question?")
+				})
+			})
+			return output, answer
+		}
+
+		It("returns true when 'y' or 'Y' is pressed", func() {
+			output, answer := PromptForInput("y\n")
+			Expect(output).To(ContainElement(ContainSubstring("Is this a dumb question? (y/n)")))
+			Expect(answer).To(BeTrue())
+			output, answer = PromptForInput("Y\n")
+			Expect(output).To(ContainElement(ContainSubstring("Is this a dumb question? (y/n)")))
+			Expect(answer).To(BeTrue())
+		})
+		It("returns false when anything else is pressed", func() {
+			output, answer := PromptForInput("n\n")
+			Expect(output).To(ContainElement(ContainSubstring("Is this a dumb question? (y/n)")))
+			Expect(answer).To(BeFalse())
+			output, answer = PromptForInput("N\n")
+			Expect(output).To(ContainElement(ContainSubstring("Is this a dumb question? (y/n)")))
+			Expect(answer).To(BeFalse())
+			output, answer = PromptForInput("\n")
+			Expect(output).To(ContainElement(ContainSubstring("Is this a dumb question? (y/n)")))
+			Expect(answer).To(BeFalse())
+			output, answer = PromptForInput("askdjfkdj\n")
+			Expect(output).To(ContainElement(ContainSubstring("Is this a dumb question? (y/n)")))
+			Expect(answer).To(BeFalse())
+			output, answer = PromptForInput("kdjf kdjf\n")
+			Expect(output).To(ContainElement(ContainSubstring("Is this a dumb question? (y/n)")))
+			Expect(answer).To(BeFalse())
 		})
 	})
 })
